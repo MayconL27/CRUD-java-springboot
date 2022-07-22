d8f6d2d11d81d03e150dc41812c3d3944779958a
MayconL27 <mayconleandro2008@gmail.com>
2022-07-22 11:19:36 -0300

    apis finalizadas

--- src/main/java/com/springbootcrud/app/controller/GreentingsController.java
+++ src/main/java/com/springbootcrud/app/controller/GreentingsController.java
@@ -54,5 +54,62 @@ public class GreentingsController {
         return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); // Rertorna a lista em JSON
 
     }
+
+    @PostMapping(value = "salvar") /* mapeia a url */
+	@ResponseBody /* Descricao da resposta */
+	public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) { /* Recebe os dados para salvar */
+
+		Usuario user = usuarioRepository.save(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+
+	}
+	
+	
+	@PutMapping(value = "atualizar") /* mapeia a url */
+	@ResponseBody /* Descricao da resposta */
+	public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) { /* Recebe os dados para salvar */
+		
+		if (usuario.getId() == null) {
+			return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);
+		}
+
+		Usuario user = usuarioRepository.saveAndFlush(usuario);
+
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+
+	}
+	
+	
+	@DeleteMapping(value = "delete") /* mapeia a url */
+	@ResponseBody /* Descricao da resposta */
+	public ResponseEntity<String> delete(@RequestParam Long iduser) { /* Recebe os dados para delete */
+
+		usuarioRepository.deleteById(iduser);
+
+		return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK);
+
+	}
+	
+	
+	@GetMapping(value = "buscaruserid") /* mapeia a url */
+	@ResponseBody /* Descricao da resposta */
+	public ResponseEntity<Usuario> buscaruserid(@RequestParam(name = "iduser") Long iduser) { /* Recebe os dados para consultar */
+
+		Usuario usuario = usuarioRepository.findById(iduser).get();
+
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+
+	}
+		
+	@GetMapping(value = "buscarPorNome") /* mapeia a url */
+	@ResponseBody /* Descricao da resposta */
+	public ResponseEntity<List<Usuario>> buscarPorNome(@RequestParam(name = "name") String name) { /* Recebe os dados para consultar */
+
+		List<Usuario> usuario = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+
+		return new ResponseEntity<List<Usuario>>(usuario, HttpStatus.OK);
+
+	}
     
 }
--- src/main/java/com/springbootcrud/app/repository/UsuarioRepository.java
+++ src/main/java/com/springbootcrud/app/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package com.springbootcrud.app.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springbootcrud.app.model.Usuario;
@@ -8,4 +11,7 @@ import com.springbootcrud.app.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
+    @Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	List<Usuario> buscarPorNome(String name);
+
 }
